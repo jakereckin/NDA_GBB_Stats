9e1f1efd3691f01e03f0cdb42f561d4ead7c16e7
jreckin <>
2024-04-30 17:53:43 -0500

    Update 4_Add_Games.py

--- pages/4_Add_Games.py
+++ pages/4_Add_Games.py
@@ -9,7 +9,7 @@ from streamlit_gsheets import GSheetsConnection
 from functions import utils as ut
 pd.options.mode.chained_assignment = None
 
-
+@st.cache_resource
 def load_data():
     conn = st.connection('gsheets', 
                          type=GSheetsConnection
@@ -17,16 +17,19 @@ def load_data():
     games = conn.read(worksheet='games')
     return conn, games
 
-conn, games = load_data()
-save = st.button('Save')
-edited_df = st.data_editor(games, 
-                           num_rows='dynamic', 
-                           key='data_editor'
-)
-if save:
-    conn.update(data=edited_df,
-                worksheet='games'
+password = st.text_input(label='Password',
+                         type='password')
+if password == st.secrets['connections.gsheets']['PAGE_PASSWORD']:
+    conn, games = load_data()
+    save = st.button('Save')
+    edited_df = st.data_editor(games, 
+                            num_rows='dynamic', 
+                            key='data_editor'
     )
-    st.write('Added to DB!')
-    time.sleep(2)
-    st.rerun()
+    if save:
+        conn.update(data=edited_df,
+                    worksheet='games'
+        )
+        st.write('Added to DB!')
+        time.sleep(2)
+        st.rerun()
