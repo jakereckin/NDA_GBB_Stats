63ade090a359dd2a696d917e5596c87707010372
jreckin <>
2024-09-26 12:58:39 -0500

    new main page

--- Home.py
+++ Home.py
@@ -13,8 +13,8 @@ pd.options.mode.chained_assignment = None
 
 
 st.session_state.temp_df = []
-st.header('NDA GBB Deep Basketball Numbers')
-st.subheader('Created by Jake Reckin')
-st.caption('Jake is the VP of Deep Basketball Analytics/5th Grade Coach/Katelyn\'s Future Hubby (title pending)')
-image = Image.open('BOYS_BEING_BOYS.jpg')
+st.header('NDA GBB Analytics')
+#st.subheader('Created by Jake Reckin')
+#st.caption('Jake is the VP of Deep Basketball Analytics/5th Grade Coach/Katelyn\'s Future Hubby (title pending)')
+image = Image.open('NDA_LOGO.jpg')
 st.image(image)
