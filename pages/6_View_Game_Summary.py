c5f4860e1c0b4c96397c96c3d6719a26d253cbb5
jakereckin <111083093+jakereckin@users.noreply.github.com>
2025-02-06 17:16:33 -0600

    Update 6_View_Game_Summary.py

--- pages/6_View_Game_Summary.py
+++ pages/6_View_Game_Summary.py
@@ -201,7 +201,8 @@ if season_list:
         st.text(body='Team Level Data')
         st.dataframe(
             data=team_data, use_container_width=True, hide_index=True,
-            column_config={'Opponent': {'max_width': 200}}
+            column_config={'Opponent': {'max_width': 200},
+                           'Offensive Efficency': {'max_width': 100},
         )
 
         data = st.radio(
