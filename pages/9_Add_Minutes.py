bf445821f616f93e48c52d1ec4e5d77653560079
jakereckin <111083093+jakereckin@users.noreply.github.com>
2025-10-30 19:38:44 -0500

    Update 9_Add_Minutes.py

--- pages/9_Add_Minutes.py
+++ pages/9_Add_Minutes.py
@@ -150,15 +150,21 @@ if password == st.secrets['page_password']['PAGE_PASSWORD']:
             value=0,
             step=1
         )
+    first_half_time = 36 * 60
+    second_half_time = 18 * 60
     if half == 2:
-        time_in = minutes * 60 + seconds
+        half_time_in = minutes * 60 + seconds
+        time_in = second_half_time - half_time_in
     else:
-        time_in = (18 + minutes) * 60 + seconds
+        half_time_in = minutes * 60 + seconds
+        time_in = first_half_time - half_time_in
 
     if half_out == 2:
-        time_out = minutes_out * 60 + seconds_out
+        half_time_out = minutes_out * 60 + seconds_out
+        time_out = second_half_time - half_time_out
     else:
-        time_out = (18 + minutes_out) * 60 + seconds_out
+        half_time_out = minutes_out * 60 + seconds_out
+        time_out = first_half_time - half_time_out
 
 
     add_minutes = st.button(label='Add Minutes')
