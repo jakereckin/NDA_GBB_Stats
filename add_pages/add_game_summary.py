4dcabb9ccbc23b104825c69d56199d775d4f4b29
jakereckin <111083093+jakereckin@users.noreply.github.com>
2026-01-24 07:42:18 -0600

    Update add_game_summary.py

--- add_pages/add_game_summary.py
+++ add_pages/add_game_summary.py
@@ -205,7 +205,7 @@ with st.form(key='Game Data', clear_on_submit=False):
         fouls_val = st.number_input(
             label='Fouls',
             min_value=0,
-            max_value=5,
+            max_value=5, 
             value=this_player_val['FOULS'].iloc[0]
         )
     save = st.form_submit_button(label='Save')
